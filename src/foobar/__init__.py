1a1e7a7a7b6ca589273ad64873657811c0d27fb8
Arto Bendiken <arto@bendiken.net>
2020-05-05 17:15:41 +0300

    Created the repository.

---
+++ src/foobar/__init__.py
@@ -0,0 +1,9 @@
+# This is free and unencumbered software released into the public domain.
+
+"""Foobar."""
+
+import sys
+
+assert sys.version_info >= (3, 6), "Foobar requires Python 3.6+"
+
+__all__ = []
